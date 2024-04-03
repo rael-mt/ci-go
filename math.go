ecd705e5af3cfc6a237434e0094c35c47b74c19b
RaelMartinss <raelpgm2@gmail.com>
2024-04-03 00:30:06 -0300

    feat: add mult

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Soma(10, 10))
+	fmt.Println(Mult(10, 10))
 }
 
-func Soma(a int, b int) int {
-	return a + b
+func Mult(a int, b int) int {
+	return a * b
 }
