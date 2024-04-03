fc6d86e89af8c2db11a2214f8bdfc72782d03ca4
RaelMartinss <raelpgm2@gmail.com>
2024-04-03 00:32:59 -0300

    feat: :recycle: refacto code

--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSoma(t *testing.T) {
 	total := Mult(15, 15)
 
-	if total != 30 {
+	if total != 225 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperando: %d", total, 30)
 	}
 }
